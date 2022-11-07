af8f0a502130977d6c7999ac7789893354c82b49
unknown <frexcho998@gmail.com>
2022-11-07 22:37:44 +0200

    added delete and create endpoints for the categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -7,7 +7,7 @@ const Category = require('../models/category');
 router.get('/', async (req, res) => {
     try {
         if (req.query.productId) {
-            res.send(await Category.find({products: req.query.productId}))
+            res.send(await Category.find({ products: req.query.productId }))
             return;
         }
         res.send(await Category.find());
@@ -30,8 +30,41 @@ router.get('/:id', async (req, res) => {
         }
         res.send(currentProduct);
     } catch (err) {
-        res.status(500).json({ error: err.message })
+        res.status(500).json({ error: err.message });
     }
 });
+
+//POST
+router.post('/', async (req, res) => {
+    try {
+        const newCategory = await Category.create({
+            name: req.body.name
+        })
+        res.status(201).send(newCategory);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+//DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(404).send({ message: 'Not Found!' });
+            return;
+        }
+        let currentCategory = await Category.findById(id);
+        if (!currentCategory) {
+            res.status(404).send({ message: 'Not Found!' });
+            return;
+        }
+        await Category.deleteOne({ _id: id });
+        await Product.updateMany({ _id: currentCategory.products }, { $pullAll: { categies: [{ _id: id }] } });
+        res.status(204).send({ message: "Deleted successfuly." });
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
 
 module.exports = router;
