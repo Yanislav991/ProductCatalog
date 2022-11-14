321554319b51c32f654905e929d373f516f33d47
unknown <frexcho998@gmail.com>
2022-11-14 22:46:03 +0200

    added product card component

---
+++ client/product-catalog/src/components/Card/index.js
@@ -0,0 +1,20 @@
+const Card = (props) => {
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src="https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png" alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{props.item.name}</div>
+                <p className="text-gray-700 text-base">
+                    {props.item.description}
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                {props.item.categories.map(((category) => (
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{category.name}</span>
+                )))}
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- client/product-catalog/src/components/Catalog/index.js
+++ client/product-catalog/src/components/Catalog/index.js
@@ -1,4 +1,5 @@
 import style from './Catalog.module.css'
+import Card from '../Card';
 import { get } from "../../util/data";
 
 let data = [];
@@ -9,18 +10,7 @@ const Catalog = (props) => {
     return (
         <div className={style.catalog}>
             {data.map(((item) => (
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <img className="w-full" src="https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png" alt="Sunset in the mountains" />
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">{item.name}</div>
-                        <p className="text-gray-700 text-base">
-                            {item.description}
-                        </p>
-                    </div>
-                    <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#test</span>
-                    </div>
-                </div>
+                <Card item={item} />
             )))}
         </div>
     );
